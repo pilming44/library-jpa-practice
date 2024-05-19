fa38a50f21ba6465b56439a8ecd4e480ce6dda30
pilming44 <pilming44@naver.com>
2024-05-19 17:42:03 +0900

    대여기간 리턴 함수 테스트코드 추가

---
+++ src/test/java/com/jpa/library/util/LoanPeriodUtilTest.java
@@ -0,0 +1,18 @@
+package com.jpa.library.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+class LoanPeriodUtilTest {
+
+    @ParameterizedTest
+    @CsvSource({"1, 7", "2, 7", "3, 10", "4, 10", "5, 10", "6, 14", "10, 14"})
+    @DisplayName("총 보유 권수에따른 대여기간")
+    void 대여기간(int totalQuantity, int loanDays) {
+        assertThat(LoanPeriodUtil.getLoanDays(totalQuantity)).isEqualTo(loanDays);
+    }
+
+}
