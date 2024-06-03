6034e76e054fd2a49b340673e7a21c6e4d66924e
pilming44 <pilming44@naver.com>
2024-06-03 15:47:20 +0900

    Book 테스트 추가

--- src/test/java/com/jpa/library/entity/BookTest.java
+++ src/test/java/com/jpa/library/entity/BookTest.java
@@ -23,8 +23,8 @@ class BookTest {
     public void setUp() {
         author = new Author("Joshua Bloch");
         publisher = new Publisher("Addison-Wesley");
-        borrowableBook = new Book("Effective Java", author, publisher, BookStatus.AVAILABLE, 1);
-        unavailableBook = new Book("InEffective Java", author, publisher, BookStatus.UNAVAILABLE, 5);
+        borrowableBook = Book.createBook("Effective Java", author, publisher, BookStatus.IN_STOCK, 1);
+        unavailableBook = Book.createBook("InEffective Java", author, publisher, BookStatus.OUT_OF_STOCK, 0);
     }
 
     @Test
@@ -52,7 +52,7 @@ class BookTest {
         borrowableBook.loan(bookLoanForm, 0);
 
         //then
-        assertThat(borrowableBook.getStatus()).isEqualTo(BookStatus.UNAVAILABLE);
+        assertThat(borrowableBook.getStatus()).isEqualTo(BookStatus.OUT_OF_STOCK);
     }
 
     @ParameterizedTest
@@ -60,7 +60,7 @@ class BookTest {
     @DisplayName("총 보유권수에따른 대여기간 적용")
     void 대여기간_적용(int totalQuantity, int loanDays) {
         //given
-        Book book = new Book("Effective Java", author, publisher, BookStatus.AVAILABLE, totalQuantity);
+        Book book = Book.createBook("Effective Java", author, publisher, BookStatus.IN_STOCK, totalQuantity);
         BookLoanForm bookLoanForm = new BookLoanForm();
         bookLoanForm.setBorrowerName("최재현");
         bookLoanForm.setLoanDate(LocalDateTime.parse("2024-05-19T15:30:00"));
@@ -70,7 +70,45 @@ class BookTest {
 
         //then
         assertThat(loan.getDueDate()).isEqualTo(LocalDateTime.parse("2024-05-19T15:30:00").plusDays(loanDays));
+    }
 
+    @Test
+    void 보유중_정상_생성() {
+        //when
+        Book book = Book.createBook("보유 정상 타이틀", author, publisher, BookStatus.IN_STOCK, 5);
 
+        //then
+        assertThat(book).isNotNull();
+        assertThat(book.getTitle()).isEqualTo("보유 정상 타이틀");
+        assertThat(book.getStatus()).isEqualTo(BookStatus.IN_STOCK);
+        assertThat(book.getTotalQuantity()).isEqualTo(5);
+    }
+
+    @Test
+    void 보유중_예외_발생() {
+        assertThatThrownBy(() -> {
+            Book.createBook("보유 예외 타이틀", author, publisher, BookStatus.IN_STOCK, 0);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("보유중 상태일 땐 책의 수량은 0보다 커야 합니다.");
+    }
+
+    @Test
+    void 미보유_정상_생성() {
+        //when
+        Book book = Book.createBook("미보유 정상 타이틀", author, publisher, BookStatus.OUT_OF_STOCK, 0);
+
+        //then
+        assertThat(book).isNotNull();
+        assertThat(book.getTitle()).isEqualTo("미보유 정상 타이틀");
+        assertThat(book.getStatus()).isEqualTo(BookStatus.OUT_OF_STOCK);
+        assertThat(book.getTotalQuantity()).isEqualTo(0);
+    }
+
+    @Test
+    void 미보유_예외_발생() {
+        assertThatThrownBy(() -> {
+            Book.createBook("미보유 예외 타이틀", author, publisher, BookStatus.OUT_OF_STOCK, 5);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("미보유 상태일 땐 책의 수량은 0이어야합니다.");
     }
 }
